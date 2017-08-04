0fe5598de423f76672c84b9a9f4209b966904034
Benjamen Mai <user@Users-MacBook-Air.local>
2017-08-04 20:29:00 +1000

    Initial Commit

---
+++ DIY PC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DIY PC
+//
+//  Created by User on 4/8/17.
+//  Copyright © 2017 rmit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DIY PCTests/DIY_PCTests.swift
@@ -0,0 +1,36 @@
+//
+//  DIY_PCTests.swift
+//  DIY PCTests
+//
+//  Created by User on 4/8/17.
+//  Copyright © 2017 rmit. All rights reserved.
+//
+
+import XCTest
+@testable import DIY_PC
+
+class DIY_PCTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
